a2f45d999c3f62c9fe7b57e1e7f014a7036b1375
Madeleine Williams <williams.maddie@gmail.com>
2018-10-25 12:02:58 -0400

    Delete hidden.py
    Secret!!

--- hidden.py
+++
@@ -1,9 +0,0 @@
-# Keep this file separate
-# https://apps.twitter.com/
-# Create new App and get the four strings
-def oauth():
-    return {"consumer_key": "hPUGlXDDbkL0jiDL0K7jSmkhS",
-    "consumer_secret" : "7yhXQ1Xtr8SfdmyIL4ECuERDhkWafdeP7wovbYJnu9dPoVd9gr",
-    "token_key" : "893287732037996545-0XnGQ4voEPVpBDNaFzck4A1lHAhJbXI",
-    "token_secret" : "GcCWlKN23qxWv8x7E3H7VAXFt7hfmyhyZw1kkLFGR6bkZ"}
-# Code: http://www.py4e.com/code3/hidden.py
